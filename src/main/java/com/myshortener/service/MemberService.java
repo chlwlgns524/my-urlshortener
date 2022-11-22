0be21ff3588d02b6a63af20a0b842a740ec16f31
chlwlgns524 <chlwlgns524@naver.com>
2022-11-22 14:42:54 +0900

    [refactor] email로 entity를 조회하는 중복 메소드 리팩토링 및 회원 비밀번호 불일치 시 예외 메시지 추가

--- src/main/java/com/myshortener/service/MemberService.java
+++ src/main/java/com/myshortener/service/MemberService.java
@@ -1,8 +1,8 @@
 package com.myshortener.service;
 
 import com.myshortener.controller.dto.MemberDto;
-import com.myshortener.mapper.MemberMapper;
 import com.myshortener.entity.Member;
+import com.myshortener.mapper.MemberMapper;
 import com.myshortener.repository.MemberRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,24 +14,26 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
 
-    public void createNewMember(MemberDto memberDto) {
-        memberRepository.save(memberMapper.toMember(memberDto));
+        public void createNewMember(MemberDto memberDto) {
+            memberRepository.save(memberMapper.toMember(memberDto));
+        }
+
+        public MemberDto findMemberByEmail(String email) {
+            return memberMapper.toMemberDto(this.findMemberEntityByEmail(email));
+        }
+
+        public MemberDto login(String email, String password) {
+            Member member = this.findMemberEntityByEmail(email);
+
+            if (member.getPassword().equals(password))
+                return memberMapper.toMemberDto(member);
+            else
+                throw new RuntimeException("비밀번호가 일치하지 않습니다.");
     }
 
-    public MemberDto findMemberByEmail(String email) {
-        return memberMapper.toMemberDto(
-            memberRepository.findByEmail(email)
-                .orElseThrow(RuntimeException::new));
-    }
-
-    public MemberDto login(String email, String password) {
-        Member member = memberRepository.findByEmail(email)
+    public Member findMemberEntityByEmail(String email) {
+        return memberRepository.findByEmail(email)
             .orElseThrow(() -> new RuntimeException("해당 회원이 존재하지 않습니다."));
-
-        if (member.getPassword().equals(password))
-            return memberMapper.toMemberDto(member);
-        else
-            throw new RuntimeException();
     }
 
 }
